80a21309ce0740f81e27dbee61ed72e6f9723b04
atx77 <atx77@users.noreply.github.com>
2022-11-13 16:50:47 +0100

    Add Croatian locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,28 @@
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
 
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import localeHr from '@angular/common/locales/hr';
+import { FormsModule } from '@angular/forms';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AccountPageComponent } from './account-page/account-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient} from '@angular/common/http';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { CategoryPageComponent } from './category-page/category-page.component';
+import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
+import { HomepageComponent } from './homepage/homepage.component';
 import { AddTokenInterceptor } from './interceptor/add-token.interceptor';
 import { LoginComponent } from './login/login.component';
 import { NavigationComponent } from './navigation/navigation.component';
-import { FormsModule } from '@angular/forms';
-import { CategoryPageComponent } from './category-page/category-page.component';
+import { OrderPageComponent } from './order-page/order-page.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductPageComponent } from './product-page/product-page.component';
-import { SearchPageComponent } from './search-page/search-page.component';
 import { RegisterComponent } from './register/register.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
-import { CheckoutPageComponent } from './checkout-page/checkout-page.component';
-import { OrderPageComponent } from './order-page/order-page.component';
-import { AccountPageComponent } from './account-page/account-page.component';
-import { HomepageComponent } from './homepage/homepage.component';
+import { SearchPageComponent } from './search-page/search-page.component';
+
+registerLocaleData(localeHr, 'hr')
 
 @NgModule({
   declarations: [
@@ -48,6 +52,10 @@ import { HomepageComponent } from './homepage/homepage.component';
       provide: HTTP_INTERCEPTORS,
       useClass: AddTokenInterceptor,
       multi: true
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'hr'
     }
   ],
   bootstrap: [AppComponent]
